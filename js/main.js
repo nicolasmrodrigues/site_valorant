479e027c36b4112c87920982ab950fd2bb0d04ca
Nícolas Medeiros Rodrigues <nicolasmedeiros090@gmail.com>
2023-02-03 13:14:55 -0300

    commit 22

--- js/main.js
+++ js/main.js
@@ -1,34 +1,49 @@
 $(document).ready(function() {
-    $('#telefone').mask('(00) 00000-0000')
+    let PosicaoQueScrollAparece = 0;
+
+    if (window.innerWidth < 767) {
+        PosicaoQueScrollAparece = 450;
+    } else {
+        PosicaoQueScrollAparece = 820;
+    }
+
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > PosicaoQueScrollAparece || document.documentElement.scrollTop > PosicaoQueScrollAparece) {
+            $('#top-button').css("display", "block");
+        } else {
+            $('#top-button').css("display", "none");
+        }
+    }
 
     $('#top-button').click(function (e) { 
         e.preventDefault();
-        const destino = $('h1')
-        $('html').animate({
-            scrollTop: destino.offset().top - 35
-        }, 500)
+        $('html').animate({scrollTop: 0}, 500);
         
     });
+
+    $('#telefone').mask('(00) 00000-0000');
     
     $('form').on('submit', function(e) {
-        e.preventDefault()
-        let nome = $('#nome')
-        let telefone = $('#telefone')
-        let nomeValido = false
-        let telefoneValido = false
+        e.preventDefault();
+        let nome = $('#nome');
+        let telefone = $('#telefone');
+        let nomeValido = false;
+        let telefoneValido = false;
 
         if (!nome.val() == '') {
-            nomeValido = true
+            nomeValido = true;
         }
 
         if (telefone.val().length === 15) {
-            telefoneValido = true
+            telefoneValido = true;
         }
 
         if (nomeValido && telefoneValido) {
-            $('form button').css("background-color", "green")
-            $('form button').html('SUCESSO')
-            $('form button').css("color", "white")
+            $('form button').css("background-color", "green");
+            $('form button').html('SUCESSO');
+            $('form button').css("color", "white");
 
         }
     })
